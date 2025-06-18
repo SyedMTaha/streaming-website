04701279b813b2b7525cf330e17bbf2e7edf44fe
Taha <syedmtaha03@gmail.com>
2025-06-19 00:37:46 +0500

    vercel error

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -6,7 +6,7 @@ import NavbarLogo from "../../../../components/navbarLogo";
 export default function LoginPage() {
   return (
     <div className="min-h-screen">
-      <NavbarLogo />
+      {/* <NavbarLogo /> */}
       <div className="h-[122vh] px-12 pt-12 flex items-center relative">
       <div className="relative z-10">
           <LoginForm/>
--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -1,10 +1,12 @@
+"use client"
+
 import SignupForm from "../../../../components/signupForm";
 import Navbar from "../../../../components/navbarLogo";
 
 export default function SignupPage() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="h-[122vh] px-12 pt-12 flex items-center relative">
         {/* Gradient overlay */}
         <div className="absolute inset-0 bg-gradient-to-r from-[#020D1E] via-[#020D1E]/70 to-transparent" />
